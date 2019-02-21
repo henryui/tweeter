fb81de1288019db745efa1ab5a8904ea7b800aa4
Yunsung Oh <henry.oh@utoronto.ca>
2019-02-21 11:31:14 -0500

    modify seed.js to include dotenv

--- public/scripts/seed.js
+++ public/scripts/seed.js
@@ -1,5 +1,6 @@
 const {MongoClient} = require("mongodb");
-const MONGODB_URI = "mongodb://localhost:27017/";
+require('dotenv').config();
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const tweets = [
   {
